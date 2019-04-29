3e4fa4aad2b2695c914c43dc95c962e3743d3972
mdalmao <mdalmaouy@gmail.com>
2019-04-29 15:51:46 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb://localhost/mern-crud-test';
 
-mongoose.connect(URI)
-  .then(db => console.log('Db is connected'))
-  .catch(error => console.error(error));
+const URI = "mongodb+srv://prueba:VdaOIx6tqcgTBpOz@cluster0-sgraz.mongodb.net/test";
+//const URI='mongodb://prueba:Sistemas1@172.30.243.232:27017/mern-tasks';
+
+mongoose.connect(URI, { useNewUrlParser: true })
+    .then(db => console.log('DB is connected'))
+    .catch(err => console.error(err));
 
 module.exports = mongoose;
